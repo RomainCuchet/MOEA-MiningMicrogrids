8f94fd731f96cfa0f0e9cf8b26625dc08ef8f448
Romain-CUCHET <romain.cuchet02@gmail.com>
2026-01-17 14:22:32 +0100

    Solar pannel fix angle issue and refactor class

--- components/power_production/power_production_manager.py
+++ components/power_production/power_production_manager.py
@@ -17,19 +17,9 @@ class PowerProductionManager:
         )
 
     def compute_solar_panel_power(self, ghi, dhi, bhi, t_amb, sza, azimuth):
-
-        solar_panel = SolarPanel(
-            p_stc=self.config.SP_P_STC,
-            gamma=self.config.SP_GAMMA,
-            beta=self.config.SP_BETA,
-            noct=self.config.SP_NOCT,
-            l_syst=self.config.SP_L_SYST,
-            albedo=0.2,
-        )
-        power_output = solar_panel._compute_power_output(
+        return self.solar_panel._compute_power_output(
             ghi, dhi, bhi, t_amb, sza, azimuth
         )
-        return power_output
 
     def simmulate_historical_power_production(
         self, start_time: pd.Timestamp, end_time: pd.Timestamp
@@ -38,14 +28,18 @@ class PowerProductionManager:
         df["time"] = pd.to_datetime(df["time"])
         df = df.set_index("time")
         df = df[(df.index >= start_time) & (df.index <= end_time)]
+
+        solar_azimuth, sza = compute_solar_azimuth_zenith(
+            self.config.SITE_LATITUDE, self.config.SITE_LONGITUDE, df.index
+        )
+        sza_tab = df["sza"]
+
         df["sp_power"] = self.compute_solar_panel_power(
             df["GHI"],
             df["DHI"],
             df["BHI"],
             df["T"],
             df["sza"],
-            compute_solar_azimuth_zenith(
-                self.config.SITE_LATITUDE, self.config.SITE_LONGITUDE, df.index
-            )[0],
+            solar_azimuth,
         )
         return df
--- components/power_production/solar_panel.py
+++ components/power_production/solar_panel.py
@@ -2,43 +2,60 @@ import numpy as np
 
 
 class SolarPanel:
-    def __init__(self, p_stc, gamma, beta, noct, l_syst, albedo):
+    def __init__(
+        self,
+        p_stc: float,
+        gamma: float,
+        beta: float,
+        noct: float,
+        l_syst: float,
+        albedo: float,
+        surface_azimuth: float = 180,
+    ):
         self.p_stc = p_stc
         self.gamma = gamma
-        self.beta = beta
+        self.beta = np.radians(beta)
         self.noct = noct
         self.l_syst = l_syst
-        self.p_stc = p_stc
         self.albedo = albedo
+        self.surface_azimuth = np.radians(surface_azimuth)
 
-    def _compute_bmod(self, bhi, sza, azimuth_sol):
-        cos_theta = np.cos(sza) * np.cos(self.beta) + np.sin(sza) * np.sin(
+    def _compute_power_output(
+        self,
+        ghi: float,
+        dhi: float,
+        bhi: float,
+        t_amb: float,
+        sza: float,
+        solar_azimuth: float,
+    ) -> float:
+        sza_rad = np.radians(sza)
+        solar_azimuth_rad = np.radians(solar_azimuth)
+
+        cos_theta = np.cos(sza_rad) * np.cos(self.beta) + np.sin(sza_rad) * np.sin(
             self.beta
-        ) * np.cos(azimuth_sol - 180)
-        return bhi * cos_theta / np.cos(sza)
+        ) * np.cos(solar_azimuth_rad - self.surface_azimuth)
+        cos_theta = np.clip(cos_theta, 0, 1)
 
-    def _compute_dmod(self, dhi):
-        return dhi * (1 + np.cos(self.beta)) / 2
+        cos_sza = np.cos(sza_rad)
 
-    def _compute_rmod(self, ghi):
-        return ghi * self.albedo * (1 - np.cos(self.beta)) / 2
+        with np.errstate(divide="ignore", invalid="ignore"):
+            b_mod = np.where(cos_sza > 0.05, bhi * cos_theta / cos_sza, 0)
+        b_mod = np.clip(b_mod, 0, bhi * 1.5)
 
-    def _compute_gti(self, ghi, dhi, bhi, sza, azimuth_sol):
-        dmod = self._compute_dmod(dhi)
-        rmod = self._compute_rmod(ghi)
-        bmod = self._compute_bmod(bhi, sza, azimuth_sol)
-        return bmod + dmod + rmod
+        d_mod = dhi * (1 + np.cos(self.beta)) / 2
+        r_mod = ghi * self.albedo * (1 - np.cos(self.beta)) / 2
 
-    def _compute_tcell(self, t_amb, gti):
-        return t_amb + (self.noct - 20) / 800 * gti
+        gti = np.maximum(b_mod + d_mod + r_mod, 0)
 
-    def _compute_power_output(self, ghi, dhi, bhi, t_amb, sza, azimuth_sol):
-        gti = self._compute_gti(ghi, dhi, bhi, sza, azimuth_sol)
-        tcell = self._compute_tcell(t_amb, gti)
-        p_out = (
+        t_cell = t_amb + (self.noct - 20) / 800 * gti
+
+        power_output = (
             self.p_stc
             * (gti / 1000)
-            * (1 + self.gamma * (tcell - 25))
+            * (1 + self.gamma * (t_cell - 25))
             * (1 - self.l_syst)
         )
-        return p_out
+        if power_output < 0:
+            print("Negative power output:", power_output)
+        return np.maximum(power_output, 0)
