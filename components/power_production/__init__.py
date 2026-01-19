25480cf7ff7f264b972ab7454ceab8a073f64818
yalpha <clement.naissans@gmail.com>
2026-01-19 17:24:48 +0100

    fix error importation

--- components/power_production/__init__.py
+++ components/power_production/__init__.py
@@ -1,3 +1,3 @@
 from components.power_production.solar_panel import SolarPanel
-from components.power_production.solar_panel import WindTurbine
+from components.power_production.wind_turbine import WindTurbine
 from components.power_production.power_production_manager import PowerProductionManager
